f954b6eb7dc7e810704611b33f8defe04d9b2b92
박상희 (Alex Park) <psh010209@gmail.com>
2021-12-17 19:17:43 +0900

    chore: remove package.json dependency since Window$ doesn't handle it properly

--- src/banner.ts
+++ src/banner.ts
@@ -2,19 +2,17 @@ import chalk from 'chalk';
 import Figlet from 'figlet';
 import fs from 'fs';
 
-const packageJson = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
-
 export function showBanner() {
   console.log(Figlet.textSync('ACME Wizard', 'Small Slant'));
   console.log();
   console.log(
     `${chalk.bold('ACME Wizard')} for ${chalk.bold('Stella IT Console')} - ${chalk.italic(
-      `ver. ${packageJson.version}`,
+      `ver. 0.1.0`,
     )}`,
   );
-  console.log(chalk.cyan(chalk.underline(packageJson.repository)));
+  console.log(chalk.cyan(chalk.underline("https://github.com/Stella-IT/acme-wizard")));
   console.log();
   console.log(`Copyright © ${chalk.bold(`${chalk.cyan('Stella')} ${chalk.blue('IT')} ${chalk.magenta('Inc.')}`)}`);
-  console.log(`Distributed under ${packageJson.license} License`);
+  console.log(`Distributed under MIT License`);
   console.log();
 }
